de620ec2efef9dd9c74f8b6b5497dc945ea5fff7
shivaagrwal194-hub <shivaagrwal194@gmail.com>
2026-02-06 20:33:39 +0530

    Update accuracy_reporter.py

--- python-fundamentals-assignment/accuracy_reporter.py
+++ python-fundamentals-assignment/accuracy_reporter.py
@@ -1,1 +1,9 @@
+# Ask user for accuracy value
+accuracy_input = input("Enter model accuracy: ")
+
+# Convert to float
+accuracy = float(accuracy_input)
+
+# Print using f-string
+print(f"Model accuracy is {accuracy}")
 
